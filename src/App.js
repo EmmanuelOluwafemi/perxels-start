cfe8b08a4a9c881b85fa4df546c8c6a44501f79c
EmmanuelOluwafemi <Seek9ja@gmail.com>
2021-01-02 07:47:03 +0100

    new update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import './App.css';
 import 'aos/dist/aos.css';
 
 import Home from './pages/Home';
-import Seo from './Seo';
 
 function App() {
 
   return (
     <>
-    <Seo />
     <Home data-aos='fade-up' />
     </>
   );
